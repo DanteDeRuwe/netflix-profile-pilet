6764ca9cde0c2cc37f3a2a5b32689cf5d834c9c4
Dante De Ruwe <dantederuwe@gmail.com>
2021-03-09 17:59:40 +0100

    Register profile as an extension

---
+++ src/Profile.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+const Profile: React.FC = () => (
+  <div className="UserProfile">
+    <div className="User">
+      <div className="name">Jane Doe</div>
+      <div className="image">
+        <img src="https://avataaars.io/?avatarStyle=Circle&topType=LongHairStraight&accessoriesType=Blank&hairColor=BrownDark&facialHairType=Blank&clotheType=BlazerShirt&eyeType=Default&eyebrowType=Default&mouthType=Default&skinColor=Light" />
+      </div>
+    </div>
+  </div>
+);
+export default Profile;
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,7 @@
-import * as React from 'react';
 import { PiletApi } from 'piral-tryout';
+import './style.scss';
+import Profile from './Profile';
 
 export function setup(app: PiletApi) {
-  app.showNotification('Hello from Piral!', {
-    autoClose: 2000,
-  });
-  app.registerMenu(() =>
-    <a href="https://docs.piral.io" target="_blank">Documentation</a>
-  );
-  app.registerTile(() => <div>Welcome to Piral!</div>, {
-    initialColumns: 2,
-    initialRows: 1,
-  });
+  app.registerExtension('header-right', Profile);
 }
