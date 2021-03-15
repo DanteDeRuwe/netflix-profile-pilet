1e64520ba63bb191b0fb79b27ce9fe83d6d4937d
Dante De Ruwe <dantederuwe@gmail.com>
2021-03-15 14:54:59 +0100

    Change extension name

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,6 @@ import ProfileExtension from './components/ProfileExtension';
 import ProfilePage from './components/ProfilePage';
 
 export function setup(app: PiletApi) {
-  app.registerExtension('header-right', ProfileExtension);
+  app.registerExtension('header-items', ProfileExtension);
   app.registerPage('/profile', ProfilePage);
 }
