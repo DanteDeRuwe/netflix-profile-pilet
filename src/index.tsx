31a65efc87d2326d64d082634d125953eb6214ee
Dante De Ruwe <dantederuwe@gmail.com>
2021-03-17 11:20:17 +0100

    Update references

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { PiletApi } from 'piral-tryout';
+import { PiletApi } from 'netflix-piral';
 import ProfileExtension from './components/ProfileExtension';
 import './scss/style.scss';
 
